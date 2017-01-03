dd63861d767c55dc1d9ca10a95d42f190c2cc6ea
Edward Park <ed.sh.park@gmail.com>
2017-01-02 16:41:13 -0800

    Fixed server routing for css + added feed navigation

--- server/server.js
+++ server/server.js
@@ -7,13 +7,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
-
+app.get('/style/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '../style/style.css'));
+});
 app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../bundle.js'));
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`server up and listening on ${PORT}`);
--- src/containers/feed_index.js
+++ src/containers/feed_index.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { Nav, NavItem } from 'react-bootstrap';
+
 // you can import Action Creators here ----
 
+// THINK ABOUT:
+// First tab could be a checklist of things you need to input for that day
+// weight, meal 1, meal 2, workout (if it's a lift day)
+// Next tabs could be your logs and tracking
 
 
 class FeedIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "1"
+    }
+  }
+
+  tabSelect(eventKey) {
+    event.preventDefault();
+    this.setState({
+      activeTab: eventKey
+    })
+  }
+
   render() {
     return (
       <div>
-        Main Index!
+
+        <Nav bsStyle="tabs" activeKey={this.state.activeTab} onSelect={this.tabSelect.bind(this)}>
+          <NavItem eventKey="1" title="Item">Checklist</NavItem>
+          <NavItem eventKey="2" title="Item">Weight Log</NavItem>
+          <NavItem eventKey="3" title="Item">Meal Log</NavItem>
+          <NavItem eventKey="4" title="Item">Macro Log</NavItem>
+        </Nav>
+
       </div>
     );
   }
