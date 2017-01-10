9b4760b12a5f246d49dc27ec093a7dfcf7266914
Edward Park <edwardpark@Edwards-MacBook-Pro.local>
2017-01-09 20:30:18 -0800

    Added checklist component routing as child of the feed component

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,14 @@ import App from './components/app';
 import Start from './components/start';
 import Login from './components/login';
 import FeedIndex from './containers/feed_index';
+import FeedChecklist from './components/feed_checklist';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Start} />
     <Route path="login" component={Login} />
-    <Route path="feed" component={FeedIndex} />
+    <Route path="feed" component={FeedIndex}>
+      <Route path="checklist" component={FeedChecklist} />
+    </Route>  
   </Route> 
 );
