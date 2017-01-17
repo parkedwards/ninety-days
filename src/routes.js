8d2cb8c2569cf2eb6d607f2997283f063a278ce0
Edward Park <ed.sh.park@gmail.com>
2017-01-16 21:50:10 -0800

    Feature: Adding tabbed routing references for log components

--- src/routes.js
+++ src/routes.js
@@ -7,13 +7,19 @@ import Start from './components/start';
 import Login from './components/login';
 import FeedIndex from './containers/feed_index';
 import FeedChecklist from './components/feed_checklist';
+import FeedWeight from './components/feed_weight';
+import FeedMeal from './components/feed_meal';
+import FeedMacro from './components/feed_macro';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Start} />
     <Route path="login" component={Login} />
     <Route path="feed" component={FeedIndex}>
-      <Route path="checklist" component={FeedChecklist} />
+      <Route path="/checklist" component={FeedChecklist} />
+      <Route path="/weight" component={FeedWeight} />
+      <Route path="/meal" component={FeedMeal} />
+      <Route path="/macro" component={FeedMacro} />
     </Route>  
   </Route> 
 );
