9c299f436e22e4ebb2163613a81a495733e44529
Edward Park <ed.sh.park@gmail.com>
2017-01-16 22:01:23 -0800

    Feature: Added moment.js

--- src/components/feed_checklist.js
+++ src/components/feed_checklist.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import moment from 'moment';
+
 const FeedChecklist = () => {
   return (
     <div>
-      Checklist!
+      <h2>Hello, Edward!</h2>
+      <p>The date is <span>{moment().format("MMM Do YYYY")}</span></p>
     </div>
   );
 };
