b9a9263b174432067497bdadbd82b6373af189e8
Edward Park <ed.sh.park@gmail.com>
2017-01-20 13:00:31 -0800

    feat: adding action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,8 @@
 import axios from 'axios';
+
+export const FETCH_WEIGHT = 'FETCH_WEIGHT';
+export const ADD_WEIGHT = 'ADD_WEIGHT';
+export const FETCH_MEALS = 'FETCH_MEALS';
+export const ADD_MEAL = 'ADD_MEAL';
+export const FETCH_MACROS = 'FETCH_MACROS';
+export const ADD_MACRO = 'ADD_MACRO';
