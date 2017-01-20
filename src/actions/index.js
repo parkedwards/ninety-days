ff75dd36671ff41939b822e5c7d3d40b1c8fc83d
Edward Park <ed.sh.park@gmail.com>
2017-01-20 14:41:01 -0800

    feat: added fetch weight action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,13 @@ export const FETCH_MEALS = 'FETCH_MEALS';
 export const ADD_MEAL = 'ADD_MEAL';
 export const FETCH_MACROS = 'FETCH_MACROS';
 export const ADD_MACRO = 'ADD_MACRO';
+
+export function fetchWeight() {
+  const url = `/weight`; // figure out how to store and access user sessions
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEIGHT,
+    payload: request
+  }
+}
