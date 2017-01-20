50a758292b6b4f3906fe27d596ca75c6026641f3
Edward Park <ed.sh.park@gmail.com>
2017-01-20 12:45:00 -0800

    feat: adding api gateway for data fetching on server

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
+// Serving APIs
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../index.html'));
 });
@@ -17,6 +17,19 @@ app.get('/style/style.css', (req, res) => {
 app.get('/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../bundle.js'));
 });
+
+
+// Data APIs
+app.get('/weight/:user', (req, res) => {
+  console.log('fetching weight log!');
+});
+app.get('/meal/:user', (req, res) => {
+  console.log('fetching meal log!');
+});
+app.get('/macro/:user', (req, res) => {
+  console.log('fetching macro log!');
+});
+
 
 
 
