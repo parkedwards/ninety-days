151e33f6648ec23a18f4145cc3c3b03716b8c2a0
Edward Park <ed.sh.park@gmail.com>
2017-01-24 23:28:55 -0800

    added routing for createUser + loginUser

--- server/users/userRoute.js
+++ server/users/userRoute.js
@@ -1,2 +1,7 @@
 const express = require('express');
 const userCtrl = require('./userCtrl');
+
+const userRoute = module.exports = express.Router();
+
+userRoute.post('/', userCtrl.createUser);
+userRoute.post('/login', userCtrl.loginUser);
