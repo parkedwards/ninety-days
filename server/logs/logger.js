b2963641c1be0fcf1af59bec2592299aa108d92a
Edward Park <ed.sh.park@gmail.com>
2017-01-26 21:20:02 -0800

    adding logger service (/log/logger.js)

--- server/logs/logger.js
+++ server/logs/logger.js
@@ -1,1 +1,16 @@
 const winston = require('winston');
+const path = require('path');
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({ filename: path.join(__dirname, '/debug.log'), json: false, level: 'error', colorize: true }),
+    new (winston.transports.Console)({ json: false, timestamp: true, level: 'info', colorize: true }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: path.join(__dirname, '/exceptions.log'), json: false }),
+    new (winston.transports.Console)({ json: false, timestamp: true }),
+  ],
+  exitOnError: false,
+});
+
+module.exports = logger;
