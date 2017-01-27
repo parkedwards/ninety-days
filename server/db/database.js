0def7726ff753c5314897e6b9b628ea428e6c22e
Edward Park <ed.sh.park@gmail.com>
2017-01-26 21:46:35 -0800

    adding winston logging to database service

--- server/db/database.js
+++ server/db/database.js
@@ -1,13 +1,15 @@
 const pg = require('pg');
 const config = require('config');
+const logger = require('../logs/logger');
+
 const dbConfig = config.get('db');
 // consider adding logger here
 
 const pool = new pg.Pool(dbConfig);
 
 pool.connect((err) => {
-  if (err) console.error(err);
-  console.log('connected to database!');
+  if (err) logger.err(err);
+  logger.log('connected to database!');
 });
 
 module.exports = pool;
