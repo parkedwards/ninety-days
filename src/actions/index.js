a00ae1dae013d1acd6e68991ae93db76e9c49919
Edward Park <ed.sh.park@gmail.com>
2017-01-28 18:50:01 -0800

    added 'createUser' action to action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+export const CREATE_USER = 'CREATE_USER';
+
 export const FETCH_WEIGHT = 'FETCH_WEIGHT';
 export const ADD_WEIGHT = 'ADD_WEIGHT';
 export const FETCH_MEALS = 'FETCH_MEALS';
@@ -7,12 +9,24 @@ export const ADD_MEAL = 'ADD_MEAL';
 export const FETCH_MACROS = 'FETCH_MACROS';
 export const ADD_MACRO = 'ADD_MACRO';
 
-export function fetchWeight() {
-  const url = `/weight`; // figure out how to store and access user sessions
-  const request = axios.get(url);
+export function createUser(props) {
+  const url = '/users/';
+  const request = axios.post(url, props);
 
   return {
-    type: FETCH_WEIGHT,
-    payload: request
-  }
+    type: CREATE_USER,
+    payload: request,
+  };
 }
+
+
+
+// export function fetchWeight() {
+//   const url = `/weight`; // figure out how to store and access user sessions
+//   const request = axios.get(url);
+
+//   return {
+//     type: FETCH_WEIGHT,
+//     payload: request,
+//   };
+// }
