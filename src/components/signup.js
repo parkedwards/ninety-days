293f227323895ed65562d9303d6c42c481131628
Edward Park <ed.sh.park@gmail.com>
2017-01-28 19:27:27 -0800

    imported 'createUser' action into 'SIgnUp' component

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,4 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { createUser } from '../actions/index';
 
 // Username
 // Password
@@ -7,20 +10,43 @@ import React, {Component} from 'react';
 // Email
 
 class SignUp extends Component {
+  onSubmitClick(e) {
+    e.preventDefault();
+    
+    const fieldResults = e.target.elements;
+
+    const username = fieldResults[0].value;
+    const password = fieldResults[1].value;
+    const first_name = fieldResults[2].value;
+    const last_name = fieldResults[3].value;
+    const email = fieldResults[4].value;
+
+    console.log({ username, password, first_name, last_name, email });
+
+    createUser({ 
+      username,
+      password,
+      first_name,
+      last_name,
+      email,
+    })
+  }
+
   render() {
     return (
       <div className="signup-form-container">
-        <form action="">
+        <form onSubmit={this.onSubmitClick}>
           <input type="text" className="signup-forms" placeholder="Username"></input>
           <input type="password" className="signup-forms" placeholder="Password"></input>
           <input type="text" className="signup-forms" placeholder="First Name"></input>
           <input type="text" className="signup-forms" placeholder="Last Name"></input>
           <input type="text" className="signup-forms" placeholder="Email"></input>
-          <button className="signup-forms signup-submit">Sign up</button>
+          <button className="signup-forms signup-submit" type="submit">Sign up</button>
         </form>
       </div>
     );
   }
 }
 
-export default SignUp;
+// export default SignUp;
+export default connect(null, { createUser })(SignUp);
