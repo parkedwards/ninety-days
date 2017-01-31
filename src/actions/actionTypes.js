14b80007249543994e3e0687f9a09779eb2a42c6
Edward Park <ed.sh.park@gmail.com>
2017-01-30 22:54:15 -0800

    created 'actionTypes.js' file to house serialized types

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,7 @@
+export const CREATE_USER = 'CREATE_USER';
+export const FETCH_WEIGHT = 'FETCH_WEIGHT';
+export const ADD_WEIGHT = 'ADD_WEIGHT';
+export const FETCH_MEALS = 'FETCH_MEALS';
+export const ADD_MEAL = 'ADD_MEAL';
+export const FETCH_MACROS = 'FETCH_MACROS';
+export const ADD_MACRO = 'ADD_MACRO';
