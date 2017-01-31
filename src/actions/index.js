fb5daa7c78bb69a55085ae6b963901c35a22e66b
Edward Park <ed.sh.park@gmail.com>
2017-01-30 23:35:06 -0800

    removed action types, housed in separate file

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,13 @@
 import axios from 'axios';
+import * as types from './actionTypes';
 
-export const CREATE_USER = 'CREATE_USER';
-
-export const FETCH_WEIGHT = 'FETCH_WEIGHT';
-export const ADD_WEIGHT = 'ADD_WEIGHT';
-export const FETCH_MEALS = 'FETCH_MEALS';
-export const ADD_MEAL = 'ADD_MEAL';
-export const FETCH_MACROS = 'FETCH_MACROS';
-export const ADD_MACRO = 'ADD_MACRO';
 
 export function createUser(props) {
   const url = '/users/';
   const request = axios.post(url, props);
 
   return {
-    type: CREATE_USER,
+    type: types.CREATE_USER,
     payload: request,
   };
 }
