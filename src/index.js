d4d231bf745e2f3a4ebcc69aeebdbda66457f48f
Edward Park <ed.sh.park@gmail.com>
2017-03-01 23:57:11 -0800

    making index.js entry more concise, referencing Root component

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { Router, browserHistory } from 'react-router';
+import { render } from 'react-dom';
+import store from './store';
+import Root from './containers/Root';
 
-import routes from './routes';
-import reducers from './reducers';
-import promise from 'redux-promise';
-
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
-
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes} />
-  </Provider>
-  , document.querySelector('.container'));
-
-
-// ReactDOM.render(
-//   <Provider store={createStoreWithMiddleware(reducers)}>
-//     <App />
-//   </Provider>
-//   , document.querySelector('.container'));
+render(
+  <Root store={store} />,
+  document.querySelector('.container')
+);
