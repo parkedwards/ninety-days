398c9117eeadfd19f6875709f1223346ff2a250e
Edward Park <ed.sh.park@gmail.com>
2017-03-01 23:57:34 -0800

    created Root component to import store + routes

---
+++ src/containers/Root.jsx
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from '../routes';
+
+const Root = ({ store }) => {
+  return (
+    <Provider store={store}>
+      <Router history={browserHistory} routes={routes} />
+    </Provider>
+  )
+}
